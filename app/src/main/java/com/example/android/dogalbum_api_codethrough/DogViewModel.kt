8db2050869911c5ee78cc65c4bd3b60229210f66
Ayana Bando <ayanabando@gmail.com>
2021-11-07 18:38:01 -0500

    Update DogViewModel.kt
    Add comments to viewModel

--- app/src/main/java/com/example/android/dogalbum_api_codethrough/DogViewModel.kt
+++ app/src/main/java/com/example/android/dogalbum_api_codethrough/DogViewModel.kt
@@ -9,13 +9,22 @@ import com.example.android.dogalbum_api_codethrough.network.DogPhoto
 import com.example.android.dogalbum_api_codethrough.network.DogPhotoApi
 
 class DogViewModel : ViewModel() {
+    
+    // The internal MutableLiveData that stores the status of the most recent request
     private val _dogPhoto = MutableLiveData<DogPhoto>()
+    // The external immutable LiveData for the request status
     val dogPhoto: LiveData<DogPhoto> = _dogPhoto
 
+        /**
+     * Call getNewPhoto() on init so we can display status immediately.
+     */
     init {
         getNewPhoto()
     }
 
+     /**
+     * Gets dog photos information from the Dog API Retrofit service
+     */
     fun getNewPhoto() {
         viewModelScope.launch {
             _dogPhoto.value = DogPhotoApi.retrofitService.getRandomPhoto()
