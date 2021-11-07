c8a2d5e6e2a8564c10632ed9567e447d10f368ce
Ayana Bando <ayanabando@gmail.com>
2021-11-07 18:34:44 -0500

    Update DogPhotoApiService.kt
    Add comments for api service

--- app/src/main/java/com/example/android/dogalbum_api_codethrough/network/DogPhotoApiService.kt
+++ app/src/main/java/com/example/android/dogalbum_api_codethrough/network/DogPhotoApiService.kt
@@ -7,7 +7,11 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://dog.ceo/api/"
-
+ /*
+  * Retrofit takes base URI for web services
+  * and converter factory to build webs services api.
+  * Retrofit  fetches JSON response and MoshiConverter
+  * converts response to string */
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
@@ -16,8 +20,16 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
-
+    
+/**
+ * A public interface that exposes the [getRandomPhoto] method
+ */
 interface DogPhotoApiService {
+        /**
+         *
+         * The @GET annotation indicates that the "breeds/image/random" endpoint will be requested with the GET
+         * HTTP method
+         */
     //https://dog.ceo/api/breeds/image/random
     @GET("breeds/image/random")
     suspend fun getRandomPhoto(): DogPhoto
@@ -25,6 +37,9 @@ interface DogPhotoApiService {
 }
 
 object DogPhotoApi {
+    /**
+      * A public Api object that exposes the lazy-initialized Retrofit service
+      */
     val retrofitService: DogPhotoApiService by lazy {
         retrofit.create((DogPhotoApiService::class.java))
     }
