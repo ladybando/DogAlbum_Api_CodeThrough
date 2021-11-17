50daad26055165e885d2f73695f692b238172de5
Ayana Bando <ayanaflax@yahoo.com>
2021-11-17 17:15:41 -0500

    add style to action bar'

--- app/src/main/java/com/example/android/dogalbum_api_codethrough/MainActivity.kt
+++ app/src/main/java/com/example/android/dogalbum_api_codethrough/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.android.dogalbum_api_codethrough
 
+
 import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.SearchView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +19,8 @@ import com.example.android.dogalbum_api_codethrough.model.DogViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private val viewModel: DogViewModel by viewModels()
+    private lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,19 +33,43 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController)
     }
-
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
 
+        val search = menu.findItem(R.id.app_bar_search)
+        searchView = search.actionView as SearchView
+
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        (menu.findItem(R.id.app_bar_search).actionView as SearchView).apply{
+        (searchView).apply {
             setSearchableInfo(searchManager.getSearchableInfo(componentName))
         }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(userQuery: String?): Boolean {
+                //Called when this view wants to give up focus
+                searchView.clearFocus()
+                //Returns the query string currently in the text field. checks if string is empty?
+                searchView.setQuery("", false)
+                search.collapseActionView()
+                viewModel.getPhotoByBreed(userQuery)
+                return true
+            }
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
         return true
     }
-
+    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+        R.id.favorite -> {
+            Log.i("MainActivity", "Favorites clicked!")
+            true
+        }
+        else ->{
+            super.onOptionsItemSelected(item)
+        }
+    }
 }
--- app/src/main/java/com/example/android/dogalbum_api_codethrough/network/DogPhotoApiService.kt
+++ app/src/main/java/com/example/android/dogalbum_api_codethrough/network/DogPhotoApiService.kt
@@ -2,6 +2,8 @@ package com.example.android.dogalbum_api_codethrough.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -9,11 +11,15 @@ import retrofit2.http.Path
 
 private const val BASE_URL = "https://dog.ceo/api/"
 
+private val networkLoggingInterceptor =
+    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
 private val retrofit = Retrofit.Builder()
+    .client(OkHttpClient.Builder().addInterceptor(networkLoggingInterceptor).build())
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
@@ -23,7 +29,7 @@ interface DogPhotoApiService {
     @GET("breeds/image/random/48")
     suspend fun getRandomPhoto(): DogPhoto
 
-    @GET("breed/{breed}/images/random")
+    @GET("breed/{breed}/images/random/48")
     suspend fun getPhotoByBreed(@Path("breed") breed: String): DogPhoto
 }
 
